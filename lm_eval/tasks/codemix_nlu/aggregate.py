af849ebdd8e83062ab7393855e9cfb3b548d647d
RosenZhang <RosenZhang@users.noreply.github.com>
2023-06-14 14:46:25 -0400

    update f1 score aggregation

--- lm_eval/tasks/codemix_nlu/aggregate.py
+++ lm_eval/tasks/codemix_nlu/aggregate.py
@@ -6,7 +6,5 @@ def codemix_nlu_multi_fi(items):
     preds, golds = zip(*items)
     preds = np.array(preds)
     golds = np.array(golds)
-    f11 = sklearn.metrics.f1_score(y_true=golds == 0, y_pred=preds == 0)
-    f12 = sklearn.metrics.f1_score(y_true=golds == 1, y_pred=preds == 1)
-    avg_f1 = np.mean([f11, f12])
+    avg_f1 = sklearn.metrics.f1_score(golds, preds, average='macro')
     return avg_f1
